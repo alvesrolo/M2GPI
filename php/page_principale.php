dec5bd080363ce37fd3fffeca2e175444b08c182
alvesrolo <q.alves@sfr.fr>
2019-09-26 14:06:11 +0200

    chargement 1

---
+++ php/page_principale.php
@@ -0,0 +1,37 @@
+<HTML>
+    <HEAD>
+        <LINK type="text/css" rel="stylesheet" href="../CSS/softmarket.css"/>
+        <TITLE> SoftMarket QAR</TITLE>
+        
+   </HEAD>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        Shop by Phone
+                    </div>
+                    <div class="col-md-6">
+                        My Acount 
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        logo
+                    </div>
+                    <div class="col-md-6">
+                        <div class="col-md-3">
+                            OFFICE MULTIMEDIA DESIGN DEVELOPER UTILITIES GAMES
+                        </div>
+                        <div class="col-md-3">
+                            <input type="search" class="input-sm form-control" placeholder="Recherche">
+              <             <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        
+    </body>
+</html>

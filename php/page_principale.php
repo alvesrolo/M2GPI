0966d9d303b198cf56208517aa5c7388cc96832b
alvesrolo <q.alves@sfr.fr>
2019-09-26 16:15:39 +0200

    chargement 3

--- php/page_principale.php
+++ php/page_principale.php
@@ -1,5 +1,7 @@
 <HTML>
     <HEAD>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <LINK type="text/css" rel="stylesheet" href="../CSS/softmarket.css"/>
         <TITLE> SoftMarket QAR</TITLE>
         
@@ -17,7 +19,7 @@
                 </div>
                 <div class="col-md-12">
                     <div class="col-md-6">
-                        logo
+                        <IMG  src="../IMAGES/img-01.png" alt="img-01.png"/>
                     </div>
                     <div class="col-md-6">
                         <div class="col-md-3">
@@ -25,11 +27,115 @@
                         </div>
                         <div class="col-md-3">
                             <input type="search" class="input-sm form-control" placeholder="Recherche">
-              <             <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+                            <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-12">
+                    DESIGN
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        Shop BY
+                    </div>
+                    <div class="col-md-10">
+                        <div class="col-md-12">
+                            BANNER
+                        </div>
+                        <div class="col-md-12">
+                            item
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">                             
+                                <div class="col-md-6">
+                                    photo
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="col-md-6">
+                                        NOM et prix
+                                    </div>
+                                    <div class="col-md-6">
+                                        ADD TO CARD
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">                             
+                                <div class="col-md-6">
+                                    photo1
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="col-md-6">
+                                        NOM et prix1
+                                    </div>
+                                    <div class="col-md-6">
+                                        ADD TO CARD1
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">                             
+                                <div class="col-md-6">
+                                    photo2
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="col-md-6">
+                                        NOM et prix2
+                                    </div>
+                                    <div class="col-md-6">
+                                        ADD TO CARD2
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        PAGE
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="col-md-3">
+                        <IMG  src="../IMAGES/img-14.png" alt="img-14.png"/>
+                    </div>
+                    <div class="col-md-3">
+                        <IMG  src="../IMAGES/img-15.png" alt="img-15.png"/>
+                    </div>
+                    <div class="col-md-3">
+                        <IMG  src="../IMAGES/img-16.png" alt="img-16.png"/>
+                    </div>
+                    <div class="col-md-3">
+                        <IMG  src="../IMAGES/img-17.png" alt="img-17.png"/>
+                    </div> 
+            </div> 
+            <div class="row">
+                <IMG  src="../IMAGES/img-27.png" alt="img-27.png"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <IMG  src="../IMAGES/img-19.png" alt="img-19.png"/> Facebook
+                    <IMG  src="../IMAGES/img-20.png" alt="img-20.png"/> Twitter
+                    <IMG  src="../IMAGES/img-21.png" alt="img-21.png"/> YouTube
+                    <IMG  src="../IMAGES/img-22.png" alt="img-22.png"/> RSS Feed
+                    <IMG  src="../IMAGES/img-25.png" alt="img-25.png"/>
+                    <IMG  src="../IMAGES/img-26.png" alt="img-26.png"/>
+                    <IMG  src="../IMAGES/img-23.png" alt="img-23.png"/>
+                    <IMG  src="../IMAGES/img-24.png" alt="img-24.png"/>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        2013 SoftMarket Mangento Store by emthemes.com
+                    </div>
+                    <div class="col-md-6">
+                        EN FR ES $ € £
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
 
 
         

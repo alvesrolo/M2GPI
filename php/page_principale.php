1befd52eda77f52ef7b25618794749f105c8ccb2
Unknown <dahaka18@gmail.com>
2019-09-28 14:30:28 +0200

    new menu

--- php/page_principale.php
+++ php/page_principale.php
@@ -1,143 +1,198 @@
 <HTML>
-    <HEAD>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <LINK type="text/css" rel="stylesheet" href="../CSS/softmarket.css"/>
-        <TITLE> SoftMarket QAR</TITLE>
-        
-   </HEAD>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="col-md-6">
-                        Shop by Phone
-                    </div>
-                    <div class="col-md-6">
-                        My Acount 
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="col-md-6">
-                        <IMG  src="../IMAGES/img-01.png" alt="img-01.png"/>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="col-md-3">
-                            OFFICE MULTIMEDIA DESIGN DEVELOPER UTILITIES GAMES
-                        </div>
-                        <div class="col-md-3">
-                            <input type="search" class="input-sm form-control" placeholder="Recherche">
-                            <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    DESIGN
-                </div>
-                <div class="row">
-                    <div class="col-md-2">
-                        Shop BY
-                    </div>
-                    <div class="col-md-10">
-                        <div class="col-md-12">
-                            BANNER
-                        </div>
-                        <div class="col-md-12">
-                            item
-                        </div>
-                        <div class="row">
-                            <div class="col-md-4">                             
-                                <div class="col-md-6">
-                                    photo
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="col-md-6">
-                                        NOM et prix
-                                    </div>
-                                    <div class="col-md-6">
-                                        ADD TO CARD
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">                             
-                                <div class="col-md-6">
-                                    photo1
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="col-md-6">
-                                        NOM et prix1
-                                    </div>
-                                    <div class="col-md-6">
-                                        ADD TO CARD1
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-4">                             
-                                <div class="col-md-6">
-                                    photo2
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="col-md-6">
-                                        NOM et prix2
-                                    </div>
-                                    <div class="col-md-6">
-                                        ADD TO CARD2
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        PAGE
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                    <div class="col-md-3">
-                        <IMG  src="../IMAGES/img-14.png" alt="img-14.png"/>
-                    </div>
-                    <div class="col-md-3">
-                        <IMG  src="../IMAGES/img-15.png" alt="img-15.png"/>
-                    </div>
-                    <div class="col-md-3">
-                        <IMG  src="../IMAGES/img-16.png" alt="img-16.png"/>
-                    </div>
-                    <div class="col-md-3">
-                        <IMG  src="../IMAGES/img-17.png" alt="img-17.png"/>
-                    </div> 
-            </div> 
-            <div class="row">
-                <IMG  src="../IMAGES/img-27.png" alt="img-27.png"/>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <IMG  src="../IMAGES/img-19.png" alt="img-19.png"/> Facebook
-                    <IMG  src="../IMAGES/img-20.png" alt="img-20.png"/> Twitter
-                    <IMG  src="../IMAGES/img-21.png" alt="img-21.png"/> YouTube
-                    <IMG  src="../IMAGES/img-22.png" alt="img-22.png"/> RSS Feed
-                    <IMG  src="../IMAGES/img-25.png" alt="img-25.png"/>
-                    <IMG  src="../IMAGES/img-26.png" alt="img-26.png"/>
-                    <IMG  src="../IMAGES/img-23.png" alt="img-23.png"/>
-                    <IMG  src="../IMAGES/img-24.png" alt="img-24.png"/>
-                </div>
-                <div class="col-md-12">
-                    <div class="col-md-6">
-                        2013 SoftMarket Mangento Store by emthemes.com
-                    </div>
-                    <div class="col-md-6">
-                        EN FR ES $ € £
-                    </div>
-                </div>
-
-            </div>
-
+  <HEAD>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <LINK type="text/css" rel="stylesheet" href="../CSS/softmarket.css" />
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+    <TITLE> SoftMarket QAR</TITLE>
+  </HEAD>
+  <body>
+    <header>
+      <div class="navbar">
+        <span>Shop by Phone (01) 123 456 &nbsp;</span>
+        <span class="blue-text">Live Chat With Us</span>
+        <div class="ml-auto">
+          <span class="blue-text">
+            My Account | My Cart
+          </span>
         </div>
+      </div>
+      <nav class="navbar navbar-expand-lg navbar-light ">
+        <img src="../IMAGES/img-01.png" alt="img-01.png" />
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">OFFICE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">MULTIMEDIA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">DESIGN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">DEVELOPER</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">UTILITIES</a>
+            </li>
+            <li class="nav-item mr-4">
+              <a class="nav-link" href="#">GAMES</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <!-- <input
+              class="form-control mr-sm-3"
+              type="search"
+              aria-label="Search"
+            /> -->
 
+            <div class="form-group has-search">
+                <input type="text" class="form-control" />
+                <span class="fa fa-search form-control-feedback"></span>
+            </div>
 
+          </form>
+        </div>
+      </nav>
+    </header>
 
-        
-    </body>
-</html>
+    <hr />
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          DESIGN
+        </div>
+        <div class="row">
+          <div class="col-md-2">
+            Shop BY
+          </div>
+          <div class="col-md-10">
+            <div class="col-md-12">
+              BANNER
+            </div>
+            <div class="col-md-12">
+              item
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="col-md-6">
+                  photo
+                </div>
+                <div class="col-md-6">
+                  <div class="col-md-6">
+                    NOM et prix
+                  </div>
+                  <div class="col-md-6">
+                    ADD TO CARD
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="col-md-6">
+                  photo1
+                </div>
+                <div class="col-md-6">
+                  <div class="col-md-6">
+                    NOM et prix1
+                  </div>
+                  <div class="col-md-6">
+                    ADD TO CARD1
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="col-md-6">
+                  photo2
+                </div>
+                <div class="col-md-6">
+                  <div class="col-md-6">
+                    NOM et prix2
+                  </div>
+                  <div class="col-md-6">
+                    ADD TO CARD2
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-12">
+            PAGE
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3">
+          <IMG src="../IMAGES/img-14.png" alt="img-14.png" />
+        </div>
+        <div class="col-md-3">
+          <IMG src="../IMAGES/img-15.png" alt="img-15.png" />
+        </div>
+        <div class="col-md-3">
+          <IMG src="../IMAGES/img-16.png" alt="img-16.png" />
+        </div>
+        <div class="col-md-3">
+          <IMG src="../IMAGES/img-17.png" alt="img-17.png" />
+        </div>
+      </div>
+      <div class="row">
+        <IMG src="../IMAGES/img-27.png" alt="img-27.png" />
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <IMG src="../IMAGES/img-19.png" alt="img-19.png" /> Facebook
+          <IMG src="../IMAGES/img-20.png" alt="img-20.png" /> Twitter
+          <IMG src="../IMAGES/img-21.png" alt="img-21.png" /> YouTube
+          <IMG src="../IMAGES/img-22.png" alt="img-22.png" /> RSS Feed
+          <IMG src="../IMAGES/img-25.png" alt="img-25.png" />
+          <IMG src="../IMAGES/img-26.png" alt="img-26.png" />
+          <IMG src="../IMAGES/img-23.png" alt="img-23.png" />
+          <IMG src="../IMAGES/img-24.png" alt="img-24.png" />
+        </div>
+        <div class="col-md-12">
+          <div class="col-md-6">
+            2013 SoftMarket Mangento Store by emthemes.com
+          </div>
+          <div class="col-md-6">
+            EN FR ES $ € £
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</HTML>
